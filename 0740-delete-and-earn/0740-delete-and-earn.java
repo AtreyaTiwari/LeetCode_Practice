f2b1396ceb62c7e0bb90c25db476ebaabc33942d
AtreyaTiwari <atreyatiwari102@gmail.com>
2025-12-19 20:45:17 +0530

    Time: 2 ms (99.85%), Space: 47 MB (24.52%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -2,28 +2,29 @@ class Solution {
     public int deleteAndEarn(int[] nums) {
         
         int n=nums.length;
-        Arrays.sort(nums);
-        int[] freq=new int[10001];
+        int max=-1;
+        for(int num:nums){
+            if(num>max){
+                max=num;
+            }
+        }
+        int[] freq=new int[max+1];
         for(int num:nums){
             freq[num]++;
         }
-        int[] dp=new int[nums[n-1]+1];
+        int[] dp=new int[max+1];
         Arrays.fill(dp,-1);
-        return rec(nums[0],nums,n,freq,nums[n-1],dp);
+        return rec(1,n,freq,max,dp);
 
     }
-    private static int rec(int value,int[] nums,int n,int[] freq,int max,int[] dp){
+    private static int rec(int value,int n,int[] freq,int max,int[] dp){
         if(value>max){
             return 0;
         }
         if(dp[value]!=-1) return dp[value];
         int ans=0;
-        int notChoose=rec(value+1,nums,n,freq,max,dp);
-        
-        int choose=(value*freq[value])+rec(value+2,nums,n,freq,max,dp);
-        
+        int notChoose=rec(value+1,n,freq,max,dp);
+        int choose=(value*freq[value])+rec(value+2,n,freq,max,dp);
         return dp[value]=ans=Math.max(choose,notChoose);
-        
-        
     }
 }
