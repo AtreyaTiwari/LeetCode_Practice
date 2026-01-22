cd03cbd06a7b506a331ba750d2178e5b220411a0
AtreyaTiwari <atreyatiwari102@gmail.com>
2026-01-22 11:02:53 +0530

    Time: 43 ms (69.75%), Space: 55.1 MB (65.3%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,4 +1,21 @@
 class Solution {
+    // public List<String> removeSubfolders(String[] folder) {
+    //     Arrays.sort(folder);
+
+    //     List<String> ans = new ArrayList<>();
+    //     ans.add(folder[0]);
+
+    //     for (int i = 1; i < folder.length; i++) {
+    //         String last = ans.get(ans.size() - 1);
+    //         String curr = folder[i];
+    //         if (!curr.startsWith(last + "/")) {
+    //             ans.add(curr);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    //check this 
     public List<String> removeSubfolders(String[] folder) {
         Arrays.sort(folder);
         ArrayList<String> ans=new ArrayList<>();
@@ -12,5 +29,22 @@ class Solution {
             }
         }
         return ans;
-    }   
+    }
+    private static ArrayList<String> compare(String fol,ArrayList<String> ans){
+        String check=ans.get(ans.size()-1);
+        int i=0;
+        while(i<Math.min(check.length(),fol.length())){
+            if(check.charAt(i)==fol.charAt(i)){
+                i++;
+            }
+        }
+        if(i==Math.min(check.length(),fol.length())){
+            if(i<fol.length() && fol.charAt(i)=='/'){
+                return ans;
+            }
+        }else{
+            ans.add(fol);
+        }
+        return ans;
+    } 
 }
