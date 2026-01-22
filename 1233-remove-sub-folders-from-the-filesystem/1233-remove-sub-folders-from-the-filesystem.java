eba541366f71a900acda5577ddc2abed40df9276
AtreyaTiwari <atreyatiwari102@gmail.com>
2026-01-22 11:41:02 +0530

    Time: 39 ms (87.9%), Space: 52.9 MB (92.7%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,34 +1,30 @@
 class Solution {
-
     public List<String> removeSubfolders(String[] folder) {
         Arrays.sort(folder);
         ArrayList<String> ans=new ArrayList<>();
         ans.add(folder[0]);
-        for(int i=1;i<folder.length;i++){
-            String last=ans.get(ans.size()-1);
-            String curr=folder[i];
-
-            if(!curr.startsWith(last+"/")){
-                ans.add(curr);
+        for(int j=1;j<folder.length;j++){
+            String check=ans.get(ans.size()-1);
+            String fol=folder[j];
+            int mini=Math.min(check.length(),fol.length());
+            int i=0;
+            while(i<mini && check.charAt(i)==fol.charAt(i)){
+                i++;
+            }
+            if(i==mini){
+                if(i<fol.length() && fol.charAt(i)=='/'){
+                    continue;
+                }
+                else{
+                    ans.add(fol);
+                }
+            }else{
+                ans.add(fol);
             }
         }
         return ans;
     }
-    private static ArrayList<String> compare(String fol,ArrayList<String> ans){
-        String check=ans.get(ans.size()-1);
-        int i=0;
-        while(i<Math.min(check.length(),fol.length())){
-            if(check.charAt(i)==fol.charAt(i)){
-                i++;
-            }
-        }
-        if(i==Math.min(check.length(),fol.length())){
-            if(i<fol.length() && fol.charAt(i)=='/'){
-                return ans;
-            }
-        }else{
-            ans.add(fol);
-        }
-        return ans;
-    } 
+    // private static ArrayList<String> compare(String fol,ArrayList<String> ans){
+        
+    // } 
 }
