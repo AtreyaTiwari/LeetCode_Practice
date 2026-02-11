4442ef5479fddff70152512deca0f664bd8f56c5
AtreyaTiwari <atreyatiwari102@gmail.com>
2026-02-11 12:03:52 +0530

    Time: 7 ms (61.57%), Space: 47.2 MB (64.2%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,73 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
+    
+    //bfs approach - kahn's
+    public boolean canFinish(int n,int[][] pre){
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-
-        for(int i=0;i<prerequisites.length;i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
+
+        int[] indg=new int[n];
+        for(int i=0;i<pre.length;i++){
+            adj.get(pre[i][0]).add(pre[i][1]);
+            indg[pre[i][1]]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indg[i]==0){
+                q.add(i);
+            }
+        }
+        int count=0;
         boolean[] vis=new boolean[n];
-        boolean[] pathVis=new boolean[n];
-        
-        for(int start=0;start<n;start++){
-            if(!vis[start]){
-                if(topo(start,adj,vis,pathVis)) return false;
-            }
-        }
-        return true;
-    }
-    private static boolean topo(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis,boolean[] pathVis){
-        vis[node]=true;
-        pathVis[node]=true;
+        while(!q.isEmpty()){
+            int node=q.poll();
+            count++;
+            vis[node]=true;
 
-        for(int ad:adj.get(node)){
-            if(!vis[ad]){
-                if(topo(ad,adj,vis,pathVis)) return true;
-            }else if(pathVis[ad]){
-                return true;
-            }
+            for(int ad:adj.get(node)){
+                if(!vis[ad]){
+                    indg[ad]--;
+                    if(indg[ad]==0) q.add(ad);
+                }
+            }    
         }
-        pathVis[node]=false;
-        return false;
+        return count==n;
+        
     }
+
+
+    // dfs method - cycle detection
+    // public boolean canFinish(int n, int[][] prerequisites) {
+    //     ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+    //     for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
+    //     for(int i=0;i<prerequisites.length;i++){
+    //         adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+    //     }
+    //     boolean[] vis=new boolean[n];
+    //     boolean[] pathVis=new boolean[n];
+        
+    //     for(int start=0;start<n;start++){
+    //         if(!vis[start]){
+    //             if(topo(start,adj,vis,pathVis)) return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // private static boolean topo(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis,boolean[] pathVis){
+    //     vis[node]=true;
+    //     pathVis[node]=true;
+
+    //     for(int ad:adj.get(node)){
+    //         if(!vis[ad]){
+    //             if(topo(ad,adj,vis,pathVis)) return true;
+    //         }else if(pathVis[ad]){
+    //             return true;
+    //         }
+    //     }
+    //     pathVis[node]=false;
+    //     return false;
+    // }
     
 }
