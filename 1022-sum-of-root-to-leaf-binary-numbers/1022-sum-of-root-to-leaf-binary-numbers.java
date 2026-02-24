428b64f308331e5bc6d6b74baca4e88e56d4ce98
AtreyaTiwari <atreyatiwari102@gmail.com>
2026-02-24 09:55:44 +0530

    Time: 0 ms (100%), Space: 43.9 MB (57.04%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -15,28 +15,40 @@
  */
 class Solution {
     public int sumRootToLeaf(TreeNode root) {
-        ArrayList<String> str=new ArrayList<>();
-        StringBuilder temp=new StringBuilder();
-        build(root,str,temp);
-        int ans=sum(str);
-        return ans;
+        if(root==null) return 0;
+        return sum(root,0);
     }
-    private static int sum(ArrayList<String> str){
-        int sum=0;
-        for(String st:str){
-            sum+=Integer.parseInt(st,2);
-        }
-        return sum;
+    private static int sum(TreeNode root,int curr){
+        if(root==null) return 0;
+        curr=(curr*2)+root.val;
+        if(root.left==null && root.right==null) return curr;
+        return sum(root.left,curr)+sum(root.right,curr);
     }
-    private static void build(TreeNode root,ArrayList<String> str,StringBuilder temp){
-        if(root==null) return ;
-        temp.append(root.val);
-        if(root.left==null && root.right==null){
-            str.add(temp.toString());
-        }else{
-            build(root.left,str,temp);
-            build(root.right,str,temp);
-        }
-        temp.deleteCharAt(temp.length()-1);
-    }
+
+    // brute force
+    // public int sumRootToLeaf(TreeNode root) {
+    //     ArrayList<String> str=new ArrayList<>();
+    //     StringBuilder temp=new StringBuilder();
+    //     build(root,str,temp);
+    //     int ans=sum(str);
+    //     return ans;
+    // }
+    // private static int sum(ArrayList<String> str){
+    //     int sum=0;
+    //     for(String st:str){
+    //         sum+=Integer.parseInt(st,2);
+    //     }
+    //     return sum;
+    // }
+    // private static void build(TreeNode root,ArrayList<String> str,StringBuilder temp){
+    //     if(root==null) return ;
+    //     temp.append(root.val);
+    //     if(root.left==null && root.right==null){
+    //         str.add(temp.toString());
+    //     }else{
+    //         build(root.left,str,temp);
+    //         build(root.right,str,temp);
+    //     }
+    //     temp.deleteCharAt(temp.length()-1);
+    // }
 }
