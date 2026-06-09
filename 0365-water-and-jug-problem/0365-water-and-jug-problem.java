73823801c8e1483d43c24011369eb11a8363497c
ATREYA TIWARI <atreyatiwari102@gmail.com>
2026-06-10 01:09:43 +0530

    Time: 5 ms (7.95%), Space: 42.8 MB (6.9%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,27 @@
+class Solution {
+    Set<String> set=new HashSet<>();
+    public boolean canMeasureWater(int x, int y, int target) {
+        return dfs(0,0,target,x,y);
+    }
+    private boolean dfs(int x,int y,int target,int m,int n){
+        String vis= x+","+y;
+        if(set.contains(vis)) return false;
+        if(x+y==target) return true;
+        set.add(vis);
+        boolean s1=dfs(m,y,target,m,n);
+        boolean s2=dfs(x,n,target,m,n);
+        boolean s3=dfs(0,y,target,m,n);
+        boolean s4=dfs(x,0,target,m,n);
+        boolean s5=false;
+        if(x<m){
+            int fill=Math.min(y,m-x);
+            s5=dfs(x+fill,y-fill,target,m,n);
+        }
+        boolean s6=false;
+        if(y<n){
+            int fill=Math.min(x,n-y);
+            s6=dfs(x-fill,y+fill,target,m,n);
+        }
+        return s1||s2||s3||s4||s5||s6;
+    }
+}
