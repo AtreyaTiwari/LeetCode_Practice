f644f7a64e5312df682907a57189aacbd93adee2
ATREYA TIWARI <atreyatiwari102@gmail.com>
2026-08-03 11:15:03 +0530

    Time: 5 ms (95.17%), Space: 85.6 MB (60.23%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,25 +1,42 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
-        int n=stoneValue.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,Integer.MIN_VALUE);
-        int value=solve(stoneValue,0,dp);
+        // int n=stoneValue.length;
+        // int[] dp=new int[n];
+        // Arrays.fill(dp,Integer.MIN_VALUE);
+        // int value=solve(stoneValue,0,dp);
+        int value=tabu(stoneValue);
         if(value>0) return "Alice";
         else if(value<0) return "Bob";
         else return "Tie";
     }
-    private static int solve(int[] stone,int i,int[] dp){
+    private static int tabu(int[] stone){
         int n=stone.length;
-        if(n<=i) return 0;
-        if(dp[i]!=Integer.MIN_VALUE) return dp[i];
-        int one=stone[i]- solve(stone,i+1,dp);
+        int[] dp=new int[n+1];
+        for(int i=n-1;i>=0;i--){
+            int one=stone[i]-dp[i+1];
 
-        int two=Integer.MIN_VALUE;
-        if(i+1<n) two=stone[i]+stone[i+1] -solve(stone,i+2,dp);
+            int two=Integer.MIN_VALUE;
+            if(i+1<n) two=stone[i]+stone[i+1] -dp[i+2];
 
-        int three=Integer.MIN_VALUE;
-        if(i+2<n) three=stone[i]+stone[i+1]+stone[i+2]-solve(stone,i+3,dp);
+            int three=Integer.MIN_VALUE;
+            if(i+2<n) three=stone[i]+stone[i+1]+stone[i+2]-dp[i+3];
 
-        return dp[i]=Math.max(one,Math.max(two,three));
+            dp[i]=Math.max(one,Math.max(two,three));
+        }
+        return dp[0];
     }
+    // private static int solve(int[] stone,int i,int[] dp){
+    //     int n=stone.length;
+    //     if(n<=i) return 0;
+    //     if(dp[i]!=Integer.MIN_VALUE) return dp[i];
+    //     int one=stone[i]- solve(stone,i+1,dp);
+
+    //     int two=Integer.MIN_VALUE;
+    //     if(i+1<n) two=stone[i]+stone[i+1] -solve(stone,i+2,dp);
+
+    //     int three=Integer.MIN_VALUE;
+    //     if(i+2<n) three=stone[i]+stone[i+1]+stone[i+2]-solve(stone,i+3,dp);
+
+    //     return dp[i]=Math.max(one,Math.max(two,three));
+    // }
 }
