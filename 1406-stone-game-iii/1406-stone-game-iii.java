2b6807cdbc843dc89a26e47521372a4916bfb6ae
ATREYA TIWARI <atreyatiwari102@gmail.com>
2026-08-03 11:10:14 +0530

    Time: 23 ms (59.09%), Space: 90.6 MB (55.97%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n=stoneValue.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,Integer.MIN_VALUE);
+        int value=solve(stoneValue,0,dp);
+        if(value>0) return "Alice";
+        else if(value<0) return "Bob";
+        else return "Tie";
+    }
+    private static int solve(int[] stone,int i,int[] dp){
+        int n=stone.length;
+        if(n<=i) return 0;
+        if(dp[i]!=Integer.MIN_VALUE) return dp[i];
+        int one=stone[i]- solve(stone,i+1,dp);
+
+        int two=Integer.MIN_VALUE;
+        if(i+1<n) two=stone[i]+stone[i+1] -solve(stone,i+2,dp);
+
+        int three=Integer.MIN_VALUE;
+        if(i+2<n) three=stone[i]+stone[i+1]+stone[i+2]-solve(stone,i+3,dp);
+
+        return dp[i]=Math.max(one,Math.max(two,three));
+    }
+}
