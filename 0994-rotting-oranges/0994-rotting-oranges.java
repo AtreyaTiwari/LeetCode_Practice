a0c0aeb1b2e16b86a9aaec207e5f579faec943cd
ATREYA TIWARI <atreyatiwari102@gmail.com>
2026-09-08 10:55:37 +0530

    Time: 4 ms (4.47%), Space: 44.6 MB (8.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -10,91 +10,41 @@ class Pair{
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n=grid.length;int m=grid[0].length;
-        int[][] vis=new int[n][m];
-        Queue<Pair> q=new LinkedList<>();
+        int m=grid.length;
+        int n=grid[0].length;
         int fresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        Queue<Pair> q=new LinkedList<>();
+        int[][] vis=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.add(new Pair(i,j,0));
                     vis[i][j]=2;
                 }else{
                     vis[i][j]=0;
                 }
-                if(grid[i][j]==1){
-                    fresh++;
-                }
+                if(grid[i][j]==1) fresh++;
             }
         }
+        int[] dr={-1,0,1,0};
+        int[] dc={0,1,0,-1};
         int time=0;
-        int[] drow={-1,0,1,0};
-        int[] dcol={0,1,0,-1};
-        int count=0;
         while(!q.isEmpty()){
             int r=q.peek().row;
             int c=q.peek().col;
             int t=q.peek().time;
-            time=Math.max(time,t);
             q.remove();
-
+            time=Math.max(t,time);
             for(int i=0;i<4;i++){
-                int nr=r+drow[i];
-                int nc=c+dcol[i];
-
-                if(nr<n&&nr>=0 && nc<m&&nc>=0 && grid[nr][nc]==1 && vis[nr][nc]==0){
-                    q.add(new Pair(nr,nc,t+1));
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && vis[nr][nc]==0){
                     vis[nr][nc]=2;
-                    count++;
+                    fresh--;
+                    q.add(new Pair(nr,nc,t+1));
                 }
             }
         }
-        return count==fresh?time:-1;
+        return fresh==0?time:-1;
     }
-    // public int orangesRotting(int[][] grid) {
-    //     int n=grid.length;int m=grid[0].length;
-    //     int[][] vis=new int[n][m];
-    //     //traverse grid and prepare visited
-    //     Queue<Pair> q=new LinkedList<>();
-    //     int fresh=0;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<m;j++){
-    //             if(grid[i][j]==2){
-    //                 q.add(new Pair(i,j,0));
-    //                 vis[i][j]=2;
-    //             }else{
-    //                 vis[i][j]=0;
-    //             }
-    //             if(grid[i][j]==1){
-    //                 fresh++;
-    //             }
-    //         }
-    //     }
-
-    //     int time=0;
-    //     int[] drow={-1,0,1,0};
-    //     int[] dcol={0,1,0,-1};
-    //     int count=0;
-
-    //     while(!q.isEmpty()){
-    //         int r=q.peek().row;
-    //         int c=q.peek().col;
-    //         int t=q.peek().time;
-    //         time=Math.max(time,t);
-    //         q.remove();
-            
-    //         for(int i=0;i<4;i++){
-    //             int nrow= r+drow[i];
-    //             int ncol= c+dcol[i];
-
-    //             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-    //                 q.add(new Pair(nrow,ncol,t+1));
-    //                 vis[nrow][ncol]=2;
-    //                 count++;
-    //             }
-    //         }
-    //     }
-    //     if(count!=fresh) return -1;
-    //     return time;
-    // }
 }
